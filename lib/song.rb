3245d62f0b79eca39051c0ec8fea16cda7dc351f
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 19:24:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+    
+    @@count += 1 
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @artist_count = {}
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.artists 
+    @@artists 
+  end
+  
+  # 
+    
+  
+    
+end
