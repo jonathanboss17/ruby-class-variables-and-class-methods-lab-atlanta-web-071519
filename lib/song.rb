87217dc40cb8b5c7cacab5820c367a41ab96452a
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 20:25:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,19 +53,4 @@ class Song
     
 end
 
-x = [1, 2, 3, 4, 5, 3, 1, 7, 8]
 
-
-    hash = {}
-    count = 0 
-    
-    for i in x do 
-      for j in x do 
-        if(i == j)
-          count += 1 
-          hash[i] = count
-        end 
-      end
-      count = 0
-    end
-puts hash 
